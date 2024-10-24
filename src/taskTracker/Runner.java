c6ff653a94f970a9849afe6bb09d10c5bc84f84b
timilehin223 <timilehinmustapha@gmail.com>
2024-10-24 05:22:23 +0200

    Implement task creation functionality
    - Added `Task` constructor to initialize task attributes.
    - Modified the `Runner` class to accept user input for task ID and description.
    - Added logic to create and add a new task to the task list.
    - Updated user prompts in the main method for better user experience.

--- src/taskTracker/Runner.java
+++ src/taskTracker/Runner.java
@@ -1,8 +1,44 @@
 package taskTracker;
 
-public class Runner {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the taskTracker.Task Tracker");
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Runner {
+    ArrayList <Task> tasks = new ArrayList<>();
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public static void main(String[] args) {
+        Runner taskManager = new Runner();
+
+        System.out.println("Welcome to the taskTracker.");
+        System.out.println("\n Choose an option. \n1. Create New Task\n2. Update Task\n3. Delete Task\n4. Exit");
+
+        Scanner sc = new Scanner(System.in);
+        switch(sc.nextInt()) {
+            case 1:
+                System.out.print("Enter task ID: ");
+                int id = sc.nextInt();
+                sc.nextLine();
+                System.out.print("Enter task description: ");
+                String description = sc.nextLine();
+                taskManager.addTask(new Task(id, description, Status.TO_DO, LocalDateTime.now()));
+                break;
+            case 2:
+                System.out.print("Enter task ID: ");
+                break;
+            case 3:
+                System.out.print("Enter task description: ");
+                break;
+            case 4:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Enter valid option.");
+                break;
+        }
     }
 }
--- src/taskTracker/Task.java
+++ src/taskTracker/Task.java
@@ -1,7 +1,20 @@
 package taskTracker;
 
+import java.time.LocalDateTime;
+
 public class Task {
     private int id;
     private String description;
+    private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task(int id, String description, Status status, LocalDateTime createdAt) {
+        this.id = id;
+        this.description = description;
+        this.status = Status.TO_DO;
+        this.createdAt = LocalDateTime.now();
+    }
+
 
 }
